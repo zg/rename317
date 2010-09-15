54f2b77bf9c00804a01d33e2e1134a4ed0eb5470
clienthax <clienthax@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2010-09-15 18:18:54 +0000

    Class43 - renamed to PlainTile PlainTile - COMPLETLY RENAMED :D

--- src/Ground.java
+++ src/Ground.java
@@ -18,7 +18,7 @@ public final class Ground extends Node {
     final int anInt1308;
     final int anInt1309;
     final int anInt1310;
-    public Class43 aClass43_1311;
+    public PlainTile aPlainTile_1311;
     public Class40 aClass40_1312;
     public Object1 obj1;
     public Object2 obj2;
