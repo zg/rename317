2646aa3144a5cd4351f8072657ed3b2f4ff12cc9
clienthax@gmail.com <clienthax@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2011-06-14 11:35:11 +0000

    Object4 renamed to GroundItemTile GroundItemTile 9/10 renamed (1 variable left) Object4 removed from svn repo

---
+++ src/GroundItemTile.java
@@ -0,0 +1,17 @@
+//Object4
+//GroundItemTile: a tile with a groundItem on it.
+final class GroundItemTile {
+
+    GroundItemTile()
+    {
+    }
+
+    int zPos;//45
+    int xPos;//46
+    int yPos;//47
+    Animable topGroundItem;//30_2_4_48
+    Animable secondGroundItem;
+    Animable thirdGroundItem;
+    int uid;
+    int anInt52;//todo - what is this O_O
+}
