de5a5ba6938e247e8b68a31428f51bb0d0b263dc
clienthax@gmail.com <clienthax@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2011-06-16 12:50:37 +0000

    Bunch of things in bzip2 system fixed readded back what i thort where unused variables (some debug crap for bzip from apache)

--- src/JagexFileStore.java
+++ src/JagexFileStore.java
@@ -1,14 +1,11 @@
 
-
-
-
 import java.io.*;
 
-final class JagexFileStore {
- //todo - think of new name - not all data is compressed (light232)
+final class JagexFileStore {//CacheIO or JagexFileStore..
+	
     public JagexFileStore(RandomAccessFile randomaccessfile, RandomAccessFile randomaccessfile1, int j)
     {
-        indexID = j;
+        	storeID = j;
             dataFile = randomaccessfile;
             indexFile = randomaccessfile1;
     }
@@ -18,54 +15,54 @@ final class JagexFileStore {
         try
         {
             seekTo(indexFile, i * 6);
-            int l;
-            for(int j = 0; j < 6; j += l)
+            int offset;
+            for(int idx = 0; idx < 6; idx += offset)
             {
-                l = indexFile.read(buffer, j, 6 - j);
-                if(l == -1)
+                offset = indexFile.read(buffer, idx, 6 - idx);
+                if(offset == -1)
                     return null;
             }
 
-            int i1 = ((buffer[0] & 0xff) << 16) + ((buffer[1] & 0xff) << 8) + (buffer[2] & 0xff);
-            int j1 = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
-            if(i1 < 0 || i1 > 0x7a120)
+            int fileSize = ((buffer[0] & 0xff) << 16) + ((buffer[1] & 0xff) << 8) + (buffer[2] & 0xff);
+            int sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
+            if(fileSize < 0 || fileSize > 0x7a120)
                 return null;
-            if(j1 <= 0 || (long)j1 > dataFile.length() / 520L)
+            if(sector <= 0 || (long)sector > dataFile.length() / 520L)
                 return null;
-            byte abyte0[] = new byte[i1];
-            int k1 = 0;
-            for(int l1 = 0; k1 < i1; l1++)
+            byte buf[] = new byte[fileSize];
+            int read = 0;
+            for(int l1 = 0; read < fileSize; l1++)
             {
-                if(j1 == 0)
+                if(sector == 0)
                     return null;
-                seekTo(dataFile, j1 * 520);
-                int k = 0;
-                int i2 = i1 - k1;
-                if(i2 > 512)
-                    i2 = 512;
-                int j2;
-                for(; k < i2 + 8; k += j2)
+                seekTo(dataFile, sector * 520);
+                int idx_ = 0;
+                int unread = fileSize - read;
+                if(unread > 512)
+                    unread = 512;
+                int off_;
+                for(; idx_ < unread + 8; idx_ += off_)
                 {
-                    j2 = dataFile.read(buffer, k, (i2 + 8) - k);
-                    if(j2 == -1)
+                    off_ = dataFile.read(buffer, idx_, (unread + 8) - idx_);
+                    if(off_ == -1)
                         return null;
                 }
 
-                int k2 = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
-                int l2 = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
-                int i3 = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
-                int j3 = buffer[7] & 0xff;
-                if(k2 != i || l2 != l1 || j3 != indexID)
+                int currentFile = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
+                int currentPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
+                int nextSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
+                int currentCache = buffer[7] & 0xff;
+                if(currentFile != i || currentPart != l1 || currentCache != storeID)
                     return null;
-                if(i3 < 0 || (long)i3 > dataFile.length() / 520L)
+                if(nextSector < 0 || (long)nextSector > dataFile.length() / 520L)
                     return null;
-                for(int k3 = 0; k3 < i2; k3++)
-                    abyte0[k1++] = buffer[k3 + 8];
+                for(int k3 = 0; k3 < unread; k3++)
+                    buf[read++] = buffer[k3 + 8];
 
-                j1 = i3;
+                sector = nextSector;
             }
 
-            return abyte0;
+            return buf;
         }
         catch(IOException _ex)
         {
@@ -75,100 +72,100 @@ final class JagexFileStore {
 
     public synchronized boolean put(int i, byte abyte0[], int j)
     {
-        boolean written = _put(true, j, i, abyte0);
-        if(!written)
-            written = _put(false, j, i, abyte0);
-        return written;
+        boolean exists = _put(true, j, i, abyte0);
+        if(!exists)
+            exists = _put(false, j, i, abyte0);
+        return exists;
     }
 
-    private synchronized boolean _put(boolean flag, int idx, int length, byte data[])
+    private synchronized boolean _put(boolean exists, int index, int length, byte data[])
     {
         try
         {
-            int block_position;
-            if(flag)
+            int sector;
+            if(exists)
             {
-                seekTo(indexFile, idx * 6);
-                int read_temp;
-                for(int i1 = 0; i1 < 6; i1 += read_temp)
+                seekTo(indexFile, index * 6);
+                int offset;
+                for(int i1 = 0; i1 < 6; i1 += offset)
                 {
-                    read_temp = indexFile.read(buffer, i1, 6 - i1);
-                    if(read_temp == -1)
+                    offset = indexFile.read(buffer, i1, 6 - i1);
+                    if(offset == -1)
                         return false;
                 }
 
-                block_position = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
-                if(block_position <= 0 || (long)block_position > dataFile.length() / 520L)
+                sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
+                if(sector <= 0 || (long)sector > dataFile.length() / 520L)
                     return false;
             } else
             {
-                block_position = (int)((dataFile.length() + 519L) / 520L);
-                if(block_position == 0)
-                    block_position = 1;
+                sector = (int)((dataFile.length() + 519L) / 520L);
+                if(sector == 0)
+                    sector = 1;
             }
             buffer[0] = (byte)(length >> 16);
             buffer[1] = (byte)(length >> 8);
             buffer[2] = (byte)length;
-            buffer[3] = (byte)(block_position >> 16);
-            buffer[4] = (byte)(block_position >> 8);
-            buffer[5] = (byte)block_position;
-            seekTo(indexFile, idx * 6);
+            buffer[3] = (byte)(sector >> 16);
+            buffer[4] = (byte)(sector >> 8);
+            buffer[5] = (byte)sector;
+            seekTo(indexFile, index * 6);
             indexFile.write(buffer, 0, 6);
-            int j1 = 0;
-            for(int l1 = 0; j1 < length; l1++)
+            int written = 0;
+            for(int l1 = 0; written < length; l1++)
             {
-                int i2 = 0;
-                if(flag)
+                int nextSector = 0;
+                if(exists)
                 {
-                    seekTo(dataFile, block_position * 520);
-                    int j2;
-                    int l2;
-                    for(j2 = 0; j2 < 8; j2 += l2)
+                    seekTo(dataFile, sector * 520);
+                    int idx;
+                    int off;
+                    for(idx = 0; idx < 8; idx += off)
                     {
-                        l2 = dataFile.read(buffer, j2, 8 - j2);
-                        if(l2 == -1)
+                        off = dataFile.read(buffer, idx, 8 - idx);
+                        if(off == -1)
                             break;
                     }
 
-                    if(j2 == 8)
+                    if(idx == 8)
                     {
-                        int i3 = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
-                        int j3 = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
-                        i2 = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
-                        int k3 = buffer[7] & 0xff;
-                        if(i3 != idx || j3 != l1 || k3 != indexID)
+                        int currentFile = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
+                        int currentPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
+                        nextSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
+                        int currentCache = buffer[7] & 0xff;
+                        if(currentFile != index || currentPart != l1 || currentCache != storeID)
                             return false;
-                        if(i2 < 0 || (long)i2 > dataFile.length() / 520L)
+                        if(nextSector < 0 || (long)nextSector > dataFile.length() / 520L)
                             return false;
                     }
                 }
-                if(i2 == 0)
+                if(nextSector == 0)
                 {
-                    flag = false;
-                    i2 = (int)((dataFile.length() + 519L) / 520L);
-                    if(i2 == 0)
-                        i2++;
-                    if(i2 == block_position)
-                        i2++;
+                    exists = false;
+                    nextSector = (int)((dataFile.length() + 519L) / 520L);
+                    if(nextSector == 0)
+                        nextSector++;
+                    if(nextSector == sector)
+                        nextSector++;
                 }
-                if(length - j1 <= 512)
-                    i2 = 0;
-                buffer[0] = (byte)(idx >> 8);
-                buffer[1] = (byte)idx;
+                if(length - written <= 512)
+                    nextSector = 0;
+                buffer[0] = (byte)(index >> 8);
+                buffer[1] = (byte)index;
                 buffer[2] = (byte)(l1 >> 8);
                 buffer[3] = (byte)l1;
-                buffer[4] = (byte)(i2 >> 16);
-                buffer[5] = (byte)(i2 >> 8);
-                buffer[6] = (byte)i2;
-                buffer[7] = (byte) indexID;
-                seekTo(dataFile, block_position * 520);
+                buffer[4] = (byte)(nextSector >> 16);
+                buffer[5] = (byte)(nextSector >> 8);
+                buffer[6] = (byte)nextSector;
+                buffer[7] = (byte) storeID;
+                seekTo(dataFile, sector * 520);
                 dataFile.write(buffer, 0, 8);
-                int k2 = length - j1;
-                if(k2 > 512)
-                    k2 = 512;
-                dataFile.write(data, j1, k2);
-                j1 += k2;
-                block_position = i2;
+                int unwritten = length - written;
+                if(unwritten > 512)
+                    unwritten = 512;
+                dataFile.write(data, written, unwritten);
+                written += unwritten;
+                sector = nextSector;
             }
 
             return true;
@@ -179,25 +176,25 @@ final class JagexFileStore {
         }
     }
 
-    private synchronized void seekTo(RandomAccessFile randomaccessfile, int j)
+    private synchronized void seekTo(RandomAccessFile randomaccessfile, int pos)
         throws IOException
     {
-        if(j < 0 || j > 0x3c00000)
+        if(pos < 0 || pos > 0x3c00000)
         {
-            System.out.println("Badseek - pos:" + j + " len:" + randomaccessfile.length());
-            j = 0x3c00000;
+            System.out.println("Badseek - pos:" + pos + " len:" + randomaccessfile.length());
+            pos = 0x3c00000;
             try
             {
                 Thread.sleep(1000L);
             }
             catch(Exception _ex) { }
         }
-        randomaccessfile.seek(j);
+        randomaccessfile.seek(pos);
     }
 
     private static final byte[] buffer = new byte[520];
     private final RandomAccessFile dataFile;
     private final RandomAccessFile indexFile;
-    private final int indexID;
+    private final int storeID;
 
 }
