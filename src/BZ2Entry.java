60a06eddf11d94ab40b2788fa54652b2dd907e4f
clienthax@gmail.com <clienthax@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2011-06-16 10:55:56 +0000

    Refactored most of BZIP2Decompressor and BZ2Entry

--- src/BZ2Entry.java
+++ src/BZ2Entry.java
@@ -1,65 +1,62 @@
-// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-
+//Thanks sadistic :3
 
 final class BZ2Entry
 {
 
     BZ2Entry()
     {
-        anIntArray583 = new int[256];
-        anIntArray585 = new int[257];
+        unzftab = new int[256];
+        cftab = new int[257];
         inUse = new boolean[256];
-        aBooleanArray590 = new boolean[16];
+        inUse16 = new boolean[16];
         seqToUnseq = new byte[256];
-        aByteArray592 = new byte[4096];
-        anIntArray593 = new int[16];
-        aByteArray594 = new byte[18002];
-        aByteArray595 = new byte[18002];
-        aByteArrayArray596 = new byte[6][258];
-        anIntArrayArray597 = new int[6][258];
-        anIntArrayArray598 = new int[6][258];
-        anIntArrayArray599 = new int[6][258];
-        anIntArray600 = new int[6];
+        yy = new byte[4096];
+        tmp = new int[16];
+        selector = new byte[18002];
+        selectorMtf = new byte[18002];
+        len = new byte[6][258];
+        limit = new int[6][258];
+        base = new int[6][258];
+        perm = new int[6][258];
+        minLens = new int[6];
     }
 
-  byte aByteArray563[];
-    int anInt564;
-    int anInt565;
-    int anInt566;
-    int anInt567;
-    byte aByteArray568[];
+    byte inputBuffer[];
+    int fileStartOffsets;
+    int compressedSize;
+    int junk1;
+    int junk2;
+    byte outputBuffer[];
     int anInt569;
-    int anInt570;
+    int decompressedSize;
     int anInt571;
     int anInt572;
     byte aByte573;
     int anInt574;
-    boolean aBoolean575;
-    int anInt576;
-    int anInt577;
-    int anInt578;
-    int anInt579;
-    int anInt580;
+    boolean randomized;
+    int current2Bytes;
+    int bitPos;
+    int blockSize;
+    int junk3;
+    int origPointer;
     int anInt581;
     int anInt582;
-    final int[] anIntArray583;
+    final int[] unzftab;
     int anInt584;
-    final int[] anIntArray585;
+    final int[] cftab;
     public static int anIntArray587[];
-    int nInUse;
+    int inUseOff;
     final boolean[] inUse;
-    final boolean[] aBooleanArray590;
+    final boolean[] inUse16;
     final byte[] seqToUnseq;
-    final byte[] aByteArray592;
-    final int[] anIntArray593;
-    final byte[] aByteArray594;
-    final byte[] aByteArray595;
-    final byte[][] aByteArrayArray596;
-    final int[][] anIntArrayArray597;
-    final int[][] anIntArrayArray598;
-    final int[][] anIntArrayArray599;
-    final int[] anIntArray600;
+    final byte[] yy;
+    final int[] tmp;
+    final byte[] selector;
+    final byte[] selectorMtf;
+    final byte[][] len;
+    final int[][] limit;
+    final int[][] base;
+    final int[][] perm;
+    final int[] minLens;
     int anInt601;
 }
