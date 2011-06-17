7e6b5875b184b431dade0418b5ff6ce73026e63f
peterbosc@gmail.com <peterbosc@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2011-06-17 17:28:02 +0000

    - Config.h equivalent is in Config.java - HD Floor reader - HD Data

---
+++ trunk/src/rs2/Config.java
@@ -0,0 +1,12 @@
+package rs2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Peter
+ * Date: 6/17/11
+ * Time: 6:57 PM
+ * Computer: Peterbjornx-PC.rootdomain.asn.local (192.168.178.27)
+ */
+public class Config {
+    public static final boolean LOAD_HD_FLO = true;
+}
