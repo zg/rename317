fc5f6df55fa091dcd67d77235b5b969e4d84f9e0
Tempest.RSPS@gmail.com <Tempest.RSPS@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2011-06-19 05:18:21 +0000

    fixed batch file, made it multiplatform

--- trunk/run.bat
+++ trunk/run.bat
@@ -1,2 +1,3 @@
-java -Djava.library.path=.\lib\native\windows -cp ./bin rs2.SwingUI
+@echo off
+java -cp lib/PGLEngine2_Library.jar;bin -Djava.library.path=lib/native/windows;lib/native/macosx;lib/native/linux;lib/native/solaris rs2.SwingUI
 pause
