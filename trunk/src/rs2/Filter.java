acdb1774dad5a939a0c76672dcc9656524813aaf
clienthax@gmail.com <clienthax@gmail.com@8e24507c-04af-f7fb-a80c-d8280bc5806f>
2012-04-06 23:06:53 +0000

    more sound stuff

--- trunk/src/rs2/Filter.java
+++ trunk/src/rs2/Filter.java
@@ -8,23 +8,40 @@ package rs2;
 public class Filter
 {
 
-    private float amplitude(int i, int j, float f)
+    private float decrementFactor(int i, int j, float f)
     {
-        float f1 = (float)anIntArrayArrayArray667[i][0][j] + f * (float)(anIntArrayArrayArray667[i][1][j] - anIntArrayArrayArray667[i][0][j]);
-            f1 *= 0.001525879F;
-            return 1.0F - (float)Math.pow(10D, -f1 / 20F);
+        float ratio = (float)lowpassSamples[i][0][j] + f * (float)(lowpassSamples[i][1][j] - lowpassSamples[i][0][j]);
+            ratio *= 0.001525879F;/* convert fixed- to floating-point: ratio * 100 / 65536 */
+            return 1.0F - (float)Math.pow(10D, -ratio / 20F);/* convert from ratio in dB to a reduction factor */
+            /* to calculate gain in dB we use L = 10 log A1^2/A0^2 = 20 log A1/A0
+            since ratio is a loss in dB we use L = -ratio
+            thus the amplitude ratio A1/A0 = 10^(L/20) = 10^(-ratio/20)
+            so to calculate the decrement
+            A0 - A1 = A0 * (A0 - A1)/A0 = A0 * (1 - A1/A0)
+            so this provides the factor to calculate the decrement */
     }
 
-    private float normalize(float octave)// like angular velocity or some shit
-    {//32.7032f Represents the note C1 on a piano keyboard. 
-        float frequency = 32.7032F * (float)Math.pow(2D, octave);
-        return (frequency * 3.141593F) / 11025F;
-    }
+    private float normalize(float octaves)
+    {
+    	/* the frequency for the C note is 32.7032 Hz
+        octave is the number of octaves above note C our note is */
+        float fNote = 32.7032F * (float)Math.pow(2D, octaves);/* convert from octaves to frequency based on C */
+        /* f_s = 22050 Hz is our sampling frequency
+        f_norm = f / f_s normalizes the frequency
+        2 * pi * f_norm converts it to radians */
+        return (fNote * 3.141593F) / 11025F;
+        /* (f * pi) / (f_s / 2)
+        = 2 * f * pi / f_s
+        = 2 * pi * f / f_s
+        = 2 * pi * f_norm
+        this calculates the frequency denoted in octaves from C and normalizes it in radians
+      */
+        }
 
     private float method543(float f, int i, int j)
     {
         float f1 = (float)anIntArrayArrayArray666[j][0][i] + f * (float)(anIntArrayArrayArray666[j][1][i] - anIntArrayArrayArray666[j][0][i]);
-        f1 *= 0.0001220703F;
+        f1 *= 0.0001220703F;/* convert fixed- to floating-point: f1 / 8192 */
         return normalize(f1);
     }
 
@@ -39,12 +56,12 @@ public class Filter
         }
         if(anIntArray665[i] == 0)
             return 0;
-        float f2 = amplitude(i, 0, f);
+        float f2 = decrementFactor(i, 0, f);
         aFloatArrayArray669[i][0] = -2F * f2 * (float)Math.cos(method543(f, 0, i));
         aFloatArrayArray669[i][1] = f2 * f2;
         for(int k = 1; k < anIntArray665[i]; k++)
         {
-            float f3 = amplitude(i, k, f);
+            float f3 = decrementFactor(i, k, f);
             float f4 = -2F * f3 * (float)Math.cos(method543(f, k, i));
             float f5 = f3 * f3;
             aFloatArrayArray669[i][k * 2 + 1] = aFloatArrayArray669[i][k * 2 - 1] * f5;
@@ -83,7 +100,7 @@ public class Filter
                 for(int l = 0; l < anIntArray665[k]; l++)
                 {
                     anIntArrayArrayArray666[k][0][l] = stream.g2();
-                    anIntArrayArrayArray667[k][0][l] = stream.g2();
+                    lowpassSamples[k][0][l] = stream.g2();
                 }
 
             }
@@ -94,11 +111,11 @@ public class Filter
                     if((j & 1 << i1 * 4 << j1) != 0)
                     {
                         anIntArrayArrayArray666[i1][1][j1] = stream.g2();
-                        anIntArrayArrayArray667[i1][1][j1] = stream.g2();
+                        lowpassSamples[i1][1][j1] = stream.g2();
                     } else
                     {
                         anIntArrayArrayArray666[i1][1][j1] = anIntArrayArrayArray666[i1][0][j1];
-                        anIntArrayArrayArray667[i1][1][j1] = anIntArrayArrayArray667[i1][0][j1];
+                        lowpassSamples[i1][1][j1] = lowpassSamples[i1][0][j1];
                     }
 
             }
@@ -116,13 +133,13 @@ public class Filter
     {
         anIntArray665 = new int[2];
         anIntArrayArrayArray666 = new int[2][2][4];
-        anIntArrayArrayArray667 = new int[2][2][4];
+        lowpassSamples = new int[2][2][4];
         anIntArray668 = new int[2];
     }
 
     final int[] anIntArray665;
     private final int[][][] anIntArrayArrayArray666;
-    private final int[][][] anIntArrayArrayArray667;
+    private final int[][][] lowpassSamples;
     private final int[] anIntArray668;
     private static final float[][] aFloatArrayArray669 = new float[2][8];
     static final int[][] anIntArrayArray670 = new int[2][8];
